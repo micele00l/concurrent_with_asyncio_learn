3c012ee086ad07938582943952682aa65f2aa126
micele00l <micele00l@gmail.com>
2023-11-08 16:26:17 +0800

    add chapter13

---
+++ chp13/code1314.py
@@ -0,0 +1,35 @@
+import asyncio
+from asyncio import StreamReader, StreamWriter, Event
+from asyncio.subprocess import Process
+
+
+async def output_consumer(input_ready_event: Event, stdout: StreamReader):
+    while (data := await stdout.read(1024)) != b'':
+        print(data)
+        if data.decode().endswith("Enter text to echo: "):
+            input_ready_event.set()
+
+
+async def input_writer(text_data, input_ready_event: Event, stdin: StreamWriter):
+    for text in text_data:
+        await input_ready_event.wait()
+        stdin.write(text.encode())
+        await stdin.drain()
+        input_ready_event.clear()
+
+
+async def main():
+    program = ['python3', 'code1313.py']
+    process: Process = await asyncio.create_subprocess_exec(*program,
+                                                            stdout=asyncio.subprocess.PIPE,
+                                                            stdin=asyncio.subprocess.PIPE)
+
+    text_input = ['one\n', 'two\n', 'three\n', 'four\n', 'quit\n']
+
+    input_ready_event = Event()
+    await asyncio.gather(output_consumer(input_ready_event, process.stdout),
+                         input_writer(text_input, input_ready_event, process.stdin),
+                         process.wait())
+
+
+asyncio.run(main())
